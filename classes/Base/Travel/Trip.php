b4ad5ed80ae52373bd3bdb31a7f64b700eb4ab69
Greg Magarshak <greg@qbix.com>
2025-11-07 15:48:08 -0500

    Regenerated models

--- classes/Base/Travel/Trip.php
+++ classes/Base/Travel/Trip.php
@@ -129,7 +129,6 @@ abstract class Base_Travel_Trip extends Db_Row
 			}
 			$fields = implode(',', $fieldNames);
 		}
-		$alias = isset($alias) ? ' '.$alias : '';
 		$q = self::db()->select($fields, self::table(true, $alias));
 		$q->className = 'Travel_Trip';
 		return $q;
